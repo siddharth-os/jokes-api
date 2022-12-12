56aa478472aec0df42400ce99e49289dd9bcc817
siddharth-os <siddharth.gupta4244@gmail.com>
2022-12-12 09:22:52 +0530

    removed faltu files and update port number

--- app.js
+++ app.js
@@ -29,6 +29,6 @@ app.get("/",(req,res)=>{
     // res.sendFile(__dirname+"/one-liner.html");
 });
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Server is Listening at port 3000.");
 });
